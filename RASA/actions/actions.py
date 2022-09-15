a1a2376f3e7c0b26b5d1dc4f39b489c3ec56da5b
Maria Araújo <maria.a.b2000@hotmail.com>
2022-09-16 00:40:43 +0100

    funcional

--- RASA/actions/actions.py
+++ RASA/actions/actions.py
@@ -14,7 +14,8 @@ from rasa_sdk.executor import CollectingDispatcher
 import requests
 from nrclex import NRCLex
 #
-#
+from rasa_sdk import Tracker, FormValidationAction
+from rasa_sdk.types import DomainDict
 #
 class ActionEmotion(Action):
 #
@@ -29,16 +30,23 @@ class ActionEmotion(Action):
          text = str (tracker.latest_message["text"])
 
          emotion = NRCLex (text)
-         emotions = []
-         for (emo,val) in emotion.top_emotions:
-             emotions.append(emo)
-
-
-         dispatcher.utter_message(text="It looks like you're feeling: {}".format(emotions))
+         print(emotion.affect_frequencies)
+         pos  = emotion.affect_frequencies['positive']
+         neg = emotion.affect_frequencies['negative']
+        
+         if pos>=neg:
+            dispatcher.utter_message(text="Fico feliz por estares a sentir-te bem!! Pareces muito positivo!!")
+         else:
+            dispatcher.utter_message(text="Parece-me que hoje estas bastante negativo! Acredita, o dia ainda vai melhorar!!")
          
          return []
 
 class ActionStudentInfo(Action):
+
+    '''
+    Esta ação na resposta deve confirma a recepção dos número de aluno. 
+    Tipo: "Recebido. Parece-me que este periodo as tuas notas estão bastantes melhores. Estas de parabéns!!"
+    '''
 
     def name(self) -> Text:
         return "action_student_info"
@@ -56,3 +64,30 @@ class ActionStudentInfo(Action):
 
         return []
 
+
+def verify_number(number):
+    f = False
+    if int(number) > 0 and int(number) < 200:
+        f = True
+    return f
+
+
+class ValidateIdentificationForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_identification_form"
+
+    def validate_student_number(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        print(slot_value)
+        # If the name is super short, it might be wrong.
+        f = verify_number(slot_value)
+        
+        if f == False:
+            dispatcher.utter_message(text="Indique um número de aluno válido. No intervalo: [0,199].")
+            return {"student_number": None}
+        return {"student_number": slot_value}
